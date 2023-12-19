97052d0972aeac77d66e012285e54a3698c2f1db
SridharanDM <109199981+SridharanDM@users.noreply.github.com>
2023-12-19 22:28:42 +0530

    feat(samp): page added

---
+++ pages/samp.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="h-screen bg-[#111928]">
+    <div class="px-5 pt-16">
+      <div class="text-3xl font-bold leading-6 text-white">
+        Here is your 3rd task
+      </div>
+
+      <div class="mt-12 text-lg font-medium leading-7 text-white">
+        Idha dialouge a system admin and tea akka kitta sonna ungalukku oru gift
+        irruku.
+      </div>
+
+      <div class="mt-10">
+        <iframe
+          class="w-full rounded-xl"
+          src="https://www.youtube.com/embed/TIAEUgnTqlQ?si=H4c0q-zZOLW1Lmro"
+          title="YouTube video player"
+          frameborder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+          allowfullscreen
+        ></iframe>
+      </div>
+    </div>
+  </div>
+</template>
